4c080100d620e36b8a40ceab940a1a51480bed15
User <User@DESKTOP-7DEACCV>
2023-01-27 12:11:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telefonen_ukazatel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phones = new Dictionary<string, string>();
+            string info = Console.ReadLine();
+            while (info != "END")
+            {
+               
+            }
+        }
+    }
+}
